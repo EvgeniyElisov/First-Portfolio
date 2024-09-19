bb201f341703a3d47e12e2897e24c503abc8a428
“Evgeniy“ <zzzzz-123@inbox.ru>
2024-09-19 15:24:43 +0300

    main structure done

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -1,13 +1,34 @@
-import React from 'react'
+import React from "react";
+import photo from "../../../assets/images/photo.webp";
+import styled from "styled-components";
+import { FlexWrapper } from "../../../components/FlexWrapper";
 
 export const Main = () => {
   return (
-    <div>
-      <div>
-        <span></span>
-        <span></span>
-      </div>
-    </div>
-  )
-}
+    <StyledMain>
+      <FlexWrapper align={"center"} justify={"space-around"}>
+        <div>
+          <span>Hi There</span>
+          <Name>I am Ivan Ivanov</Name>
+          <MainTitle>A Web Developer.</MainTitle>
+        </div>
+        <Photo src={photo} alt="Фото" />
+      </FlexWrapper>
+    </StyledMain>
+  );
+};
 
+const StyledMain = styled.div`
+  min-height: 100vh;
+  background-color: #d7acbb;
+`;
+
+const Photo = styled.img`
+  width: 350px;
+  height: 430px;
+  object-fit: cover;
+`;
+
+const MainTitle = styled.h1``;
+
+const Name = styled.h2``;

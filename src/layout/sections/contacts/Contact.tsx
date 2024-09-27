6f2e28333fbe338a905f25e95879c17dfa5fd129
“Evgeniy“ <zzzzz-123@inbox.ru>
2024-09-27 19:13:54 +0300

    contacts and slogan styles done

--- src/layout/sections/contacts/Contact.tsx
+++ src/layout/sections/contacts/Contact.tsx
@@ -1,33 +1,59 @@
 import styled from "styled-components";
 import { SectionTitle } from "../../../components/SectionTitle";
 import { Button } from "../../../components/Button";
+import { Container } from "../../../components/Container";
+import { theme } from "../../../styles/Theme";
 
 export const Contact = () => {
   return (
     <StyledContacts>
-      <SectionTitle>Contact</SectionTitle>
-      <StyledForm>
-        <Field placeholder={"Name"} />
-        <Field placeholder={"Subject"} />
-        <Field as={"textarea"} placeholder={"Message"} />
-        <Button type="submit">Send message</Button>
-      </StyledForm>
+      <Container>
+        <SectionTitle>Contact</SectionTitle>
+        <StyledForm>
+          <Field placeholder={"Name"} />
+          <Field placeholder={"Subject"} />
+          <Field as={"textarea"} placeholder={"Message"} />
+          <Button type="submit">Send message</Button>
+        </StyledForm>
+      </Container>
     </StyledContacts>
   );
 };
 
-const StyledContacts = styled.section`
-  min-height: 50vh;
-  background-color: #ddd4bd;
-`;
+const StyledContacts = styled.section``;
 
 const StyledForm = styled.form`
-  max-width: 500px;
+  max-width: 540px;
   width: 100%;
   display: flex;
   flex-direction: column;
-  gap: 10px;
+  align-items: center;
+  gap: 16px;
   margin: 0 auto;
+
+  textarea {
+    resize: none;
+    height: 155px;
+  }
 `;
 
-const Field = styled.input``;
+const Field = styled.input`
+  font-family: "Poppins", sans-serif;
+  width: 100%;
+  background-color: ${theme.colors.secondaryBg};
+  border: 1px solid ${theme.colors.borderColor};
+  padding: 7px 15px;
+  font-weight: 400;
+  font-size: 12px;
+  letter-spacing: 0.05em;
+  color: ${theme.colors.font};
+
+  &::placeholder {
+    color: ${theme.colors.placeholderColor};
+    text-transform: capitalize;
+  }
+
+  &:focus-visible {
+    outline: 1px solid ${theme.colors.borderColor};
+  }
+`;
